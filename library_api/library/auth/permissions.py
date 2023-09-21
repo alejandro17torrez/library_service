a906fb1eb75b6a97adc56ba95f46ecb3dc509119
Alejandro Torrez <ciberthingscomputinglearning@outlook.com>
2023-09-20 23:13:30 -0600

    feature: make auth features
    Serializer and viewsets to login, register, refresh token and permissions to HTTP requests

---
+++ library_api/library/auth/permissions.py
@@ -0,0 +1,21 @@
+from rest_framework.permissions import SAFE_METHODS, BasePermission
+
+
+class UserPermission(BasePermission):
+    def has_object_permission(self, request, view):
+        if request.user.is_anonymus:
+            return request.method in SAFE_METHODS
+        if view.basename in [
+            "post",
+        ]:
+            return bool(request.user and request.user.is_authenticated)
+        return False
+
+    def has_permission(self, request, view):
+        if view.basename in [
+            "post",
+        ]:
+            if request.user.is_anonymus:
+                return request.method in SAFE_METHODS
+            return bool(request.user and request.user.is_authenticated)
+        return False
