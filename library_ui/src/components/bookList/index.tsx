7729846c3107e82283f253fc78f032f977fac896
Alejandro Torrez <ciberthingscomputinglearning@outlook.com>
2023-09-22 04:45:10 -0600

    feature: page and component for list of books

--- library_ui/src/components/bookList/index.tsx
+++ library_ui/src/components/bookList/index.tsx
@@ -1,3 +1,14 @@
+import { DataView } from 'primereact/dataview';
+
+import { useBooks } from "../../hooks/useBooks";
+import { book } from './book';
+
 export default function BookList () {
-  return <p>libros</p>;
+  // @ts-ignore
+  const { books } = useBooks();
+  return (
+    <div className="card">
+      <DataView value={books} itemTemplate={book} paginator rows={5} />
+    </div>
+  );
 }
