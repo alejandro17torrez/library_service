9b33c443abe8305bb4cefc63a826edff0970d06d
Alejandro Torrez <ciberthingscomputinglearning@outlook.com>
2023-09-22 01:49:58 -0600

    settings: add primereact library

--- library_ui/src/main.tsx
+++ library_ui/src/main.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import "primereact/resources/themes/lara-light-indigo/theme.css";
+import "primereact/resources/primereact.min.css";
 import './index.css'
+
+import { AuthContextProvider } from './contexts/authContext.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <AuthContextProvider>
+      <App />
+    </AuthContextProvider>
   </React.StrictMode>,
 )
